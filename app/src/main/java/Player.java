1d2782b30373ccb7bf668f48b35a801c18c6a7bd
Alex <a.feyertag@gmail.com>
2017-04-18 14:19:35 +0200

    Merge branch 'master' of C:\Users\Alex\AndroidStudioProjects\madgame2 with conflicts.

---
+++ app/src/main/java/Player.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Alex on 18.04.2017.
+ */
+
+public class Player {
+
+    int playernr;       //Spielernummern 1-4
+
+    public Player(int playernr){
+        this.playernr=playernr;
+
+    }
+
+    public int getPlayernr() {
+        return playernr;
+    }
+
+    public void setPlayernr(int playernr) {
+        this.playernr = playernr;
+    }
+
+
+}
