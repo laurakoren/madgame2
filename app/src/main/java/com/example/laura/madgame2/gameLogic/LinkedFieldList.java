04dfcad79a5dc7ed8a631386d0b3e024176480d5
Alex <a.feyertag@gmail.com>
2017-04-23 09:25:14 +0200

    LinkedList als mögliche Alternative zu LinkedFieldCircle

---
+++ app/src/main/java/com/example/laura/madgame2/gameLogic/LinkedFieldList.java
@@ -0,0 +1,31 @@
+package com.example.laura.madgame2.gameLogic;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Alex on 23.04.2017.
+ */
+
+//ALS MÖGLICHE ALTERNATIVE ZUM LINKEDFIELDCIRCLE
+
+class LinkedFieldList {
+
+    /*LinkedList<Field> fieldList = new LinkedList<>();
+
+    int count = 1;
+
+    public void createBoard() {
+        while (count < 73) {
+            Field f = new Field(count);
+            fieldList.add(f);
+            count++;
+        }
+    }
+
+    public void setStartfield(Figure fig) {
+        fig.setStartField(fieldList.get(fig.getFigNr()));
+
+    }
+
+    */
+}
