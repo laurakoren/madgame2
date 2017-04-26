7cbe5f5d5f28fb1fc1df0d81c56160eb5ec3eacd
madmini <minigugler@gmail.com>
2017-04-26 20:51:22 +0200

    man kann was sehen

--- app/src/main/java/com/example/laura/madgame2/gameLogic/Figure.java
+++ app/src/main/java/com/example/laura/madgame2/gameLogic/Figure.java
@@ -1,31 +1,86 @@
 package com.example.laura.madgame2.gameLogic;
 
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * A single game figure/piece. Belongs to a player and is placed on a field.
  */
 public class Figure {
 
     /**
-     * the Field this Figure is occupying
+     * the Field this Figure is occupying. if this Figure is in the out area field is set to null.
      */
     private Field field;
 
+    /**
+     * this Figure's UI element
+     */
+    private View view;
+
+    /**
+     * container of settings to easily reset this figure to its outField
+     */
+    private ViewGroup.LayoutParams outField;
+
+    private Player player;
+
     // TODO: figur implementieren
     // TODO: zugehörigkeit zu spielern
-    // TODO: startfelder
 
-    public Field getField() {
+    Figure(Player player, View view) {
+        this.player = player;
+        this.view = view;
+
+        this.field = null;
+        this.outField = view.getLayoutParams();
+    }
+
+    Field getField() {
         return field;
     }
 
-    void setField(Field field) {
+    /**
+     * Moves this Figure to another field.
+     *
+     * @param field the Field to move to
+     */
+    void moveTo(Field field) {
+        // remove this from former field
+        if (this.field != null) {
+            this.field.setFigure(null);
+        }
+
+        // assign new field
         this.field = field;
-        //if (this.field.getFigure() != this)
-        //    this.field.setFigure(this);
+        field.setFigure(this);
+
+        // move figure icon
+        view.setLayoutParams(field.getViewLayout());
     }
 
-    //public Field getStartField(Figure figure){
-     //
-    //}
+    /**
+     * Moves this Figure back to its start field.
+     */
+    void returnToOut() {
+        // remove this from former field
+        if (this.field != null) {
+            this.field.setFigure(null);
+        }
 
+        // reset field and location on board
+        this.field = null;
+        view.setLayoutParams(outField);
+    }
+
+    /**
+     * Returns this Figure's start field, i.e. the Field it is placed on at the beginning of the game and returned to if kicked.
+     */
+    ViewGroup.LayoutParams getOutField(){
+        return outField;
+    }
+
+    Player getPlayer() {
+        return player;
+    }
 }
--- app/src/main/java/com/example/laura/madgame2/gameLogic/GameLogic.java
+++ app/src/main/java/com/example/laura/madgame2/gameLogic/GameLogic.java
@@ -1,5 +1,11 @@
 package com.example.laura.madgame2.gameLogic;
 
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The internal representation for the game board.
  */
@@ -7,59 +13,78 @@ package com.example.laura.madgame2.gameLogic;
 public class GameLogic {
     private LinkedFieldCircle boardPath;
 
-    public GameLogic() {
-        this.boardPath = new LinkedFieldCircle();
-        boardPath.getFirst().setFigure(new Figure());
-    }
+    private List<Player> players;
 
-    // TODO: spiellogik bauen
+    private List<List<Figure>> figures;
+
+    public GameLogic(List<Player> players, List<View> fieldViews, List<List<View>> figureViews) {
+        this.players = players;
+
+        figures = new ArrayList<>();
+        Player tmpPlayer;
+        List<Figure> tmp;
+
+        for (int i = 0; i < 4; i++) {
+            tmpPlayer = players.get(i);
+
+            if (tmpPlayer != null) {
+                tmp = new ArrayList<>();
+
+                for (int j = 0; j < 4; j++) {
+                    tmp.add(new Figure(tmpPlayer, figureViews.get(i).get(j)));
+                }
+
+                tmpPlayer.setFigures(tmp);
+                figures.add(tmp);
+            }
+        }
+
+        this.boardPath = new LinkedFieldCircle(fieldViews, players);
+    }
 
     /**
      * Moves a figure for a given distance over the board. Returns true if the move is valid and was executed and returns false else. If false is returned, the board remains unchanged.
      *
      * @param figure   the piece to move
      * @param distance the distance to move it
+     * @param player   the player making the move
      * @return true on success, false on failure
      */
-    public boolean draw(Figure figure, int distance) {
+    public boolean draw(Figure figure, int distance, Player player) {
         if (figure == null || distance < 0)
             return false;
-/*
-        if(figure.getField().equals(figure.getStartField)){ //TODO: Startfeld definieren
-            return false;
-        }else{
-            for(int i=1;i<=distance;i++){
-                figure.setField(figure.getField().next());
-                if(i<distance){
-                    //figure.getField().checkExit();       //TODO: Ausgangsknoten definieren
-                }
-                if(i==distance){
-                    kick(figure,figure.getField());
-                }
 
-                if(i==6){
-                    //TODO: bei einer 6 darf man nochmals würfeln
+        Log.d("LOGIC","msg");
+
+        Field current = figure.getField();
+
+        // determine where the figure is
+        if (current == null) {
+            // figure is in the out area
+            if (distance == 6) {
+                figure.moveTo(player.getStartField());
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            // figure is on the game board
+            for (int i = 0; i < distance; i++) {
+                current = current.next(player);
+            }
+
+            if (current.hasFigure()) {
+                Figure otherFigure = current.getFigure();
+
+                if (otherFigure.getPlayer() == player) {
+                    return false;
+                } else {
+                    otherFigure.returnToOut();
                 }
             }
-        }
 
-
-
-
-        // TODO: züge ermöglichen
-        // TODO: collision detection
-*/
-        return true;}
-
-
-    //checks whether a field already has a figure on it, sets that figure to its starting point and the new figure on the field
-    public void kick(Figure figure, Field field) {
-        if (field.hasFigure()) {
-            //field.getFigure().setField(figure.getStartField);   // TODO: Startfeld bestimmen
-            figure.setField(field);
-        } else {
-            figure.setField(field);
+            figure.moveTo(current);
+            return true;
         }
     }
-
 }
