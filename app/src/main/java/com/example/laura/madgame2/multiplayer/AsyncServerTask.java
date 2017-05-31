301126b17440cb0b98b3c920025cf6c6d0180d71
Philipp Starzacher <p.starzacher@gmail.com>
2017-05-31 10:01:08 +0200

    MiniFix

--- app/src/main/java/com/example/laura/madgame2/multiplayer/AsyncServerTask.java
+++ app/src/main/java/com/example/laura/madgame2/multiplayer/AsyncServerTask.java
@@ -3,7 +3,6 @@ package com.example.laura.madgame2.multiplayer;
 import android.os.AsyncTask;
 
 import com.example.laura.madgame2.multiplayer.update.Update;
-import com.example.laura.madgame2.multiplayer.update.UpdateChooseFigure;
 import com.example.laura.madgame2.multiplayer.update.UpdatePlayersTurn;
 
 /**
@@ -16,7 +15,7 @@ public class AsyncServerTask extends AsyncTask<Update, Void, Void> {
     protected Void doInBackground(Update... params) {
 
         if (params[0] instanceof UpdatePlayersTurn) {
-            Server.getInstance().getClient(((UpdatePlayersTurn)params[0]) .getPlayerNr()-1);
+            Server.getInstance().getClient(((UpdatePlayersTurn)params[0]) .getPlayerNr()-1).sendUpdate(params[0]);
             return null;
                     }
 
